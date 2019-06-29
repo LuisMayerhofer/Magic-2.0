467cc8ca0022f35397283d0a0d194d1a2a9ae1a5
Bobo DerTitan <iris.mayerhofer@gmx.de>
2019-06-29 23:18:45 +0200

    Added Flammenwerfer

---
+++ Magic 2.0 Unity/Assets/Scripts/AnimRight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimRight : MonoBehaviour
+{
+    public void PlayAnimFlame(bool start)
+    {
+        if (start)
+        {
+            Debug.Log("nice");
+            gameObject.GetComponent<Animator>().SetBool("FlamethrowerAktive", true);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("FlamethrowerAktive", false);
+        }
+    }
+}
--- Magic 2.0 Unity/Assets/Scripts/AttackScr.cs
+++ Magic 2.0 Unity/Assets/Scripts/AttackScr.cs
@@ -5,29 +5,38 @@ using UnityEngine;
 public class AttackScr : MonoBehaviour
 {
     public GameObject Spell1;
+    public GameObject Spell2;
+    public GameObject ArmRight;
     public Transform Spawn1;
+    public Transform Spawn2;
     public Transform ParentRotationY;
     public Transform ParentRotationX;
+    
     public float requiredLoadingTime;
     public float Speed1;
+    public float fireDelay;
+   
+        
+    
 
     private GameObject Spell1Instance;
     private float LoadingTime = 0;
     private bool Spell1aktive = false;
+    private bool Spell2aktive = false;
 
     void Awake()
     {
         
     }
 
-    
+
     void Update()
     {
         //Spell1
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0)&& !Spell2aktive)
         {
-            Spell1Instance = Instantiate(Spell1, Spawn1.position, ParentRotationY.rotation);            
+            Spell1Instance = Instantiate(Spell1, Spawn1.position, ParentRotationY.rotation);
             Spell1Instance.transform.SetParent(ParentRotationX);
 
             Spell1aktive = true;
@@ -38,10 +47,10 @@ public class AttackScr : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            if(LoadingTime>= requiredLoadingTime)
+            if (LoadingTime >= requiredLoadingTime)
             {
                 Spell1Instance.transform.parent = null;
-                Spell1Instance.GetComponent<Rigidbody>().AddForce(transform.forward * Speed1);
+                Spell1Instance.GetComponent<Rigidbody>().AddForce(Spawn1.forward * Speed1);
             }
             else
                 Destroy(Spell1Instance);
@@ -50,5 +59,31 @@ public class AttackScr : MonoBehaviour
             LoadingTime = 0;
         }
 
+        //Spell2
+        
+
+        if (Input.GetKeyDown(KeyCode.Mouse1)&& !Spell1aktive)
+        {
+            StartCoroutine(playFire());
+            Spell2aktive = true;
+            bool start;
+            ArmRight.SendMessageUpwards("PlayAnimFlame", start = true);        
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            Spell2.GetComponent<ParticleSystem>().Stop();
+            Spell2aktive = false;
+            
+            bool start;
+            ArmRight.SendMessageUpwards("PlayAnimFlame", start = false);
+        }
+
+        
+    }
+   IEnumerator playFire()
+    {
+        yield return new WaitForSeconds(fireDelay);
+        Spell2.GetComponent<ParticleSystem>().Play();
     }
 }
